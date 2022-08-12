888b80a3f1bd326598aae74f4c29f8d13ef4424f
Xiloer <83123095@qq.com>
2022-08-12 11:58:54 +0800

    V3

---
+++ V3/src/main/java/com/webserver/core/ClientHandler.java
@@ -0,0 +1,50 @@
+package com.webserver.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+/**
+ * 与客户端完成一次HTTP的交互
+ * 按照HTTP协议要求，与客户端完成一次交互流程为一问一答
+ * 因此，这里分为三步完成该工作:
+ * 1:解析请求  目的:将浏览器发送的请求内容读取并整理
+ * 2:处理请求  目的:根据浏览器的请求进行对应的处理工作
+ * 3:发送响应  目的:将服务端的处理结果回馈给浏览器
+ *
+ *
+ */
+public class ClientHandler implements Runnable{
+    private Socket socket;
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+    public void run(){
+        try {
+            InputStream in = socket.getInputStream();
+            /*
+                GET /index.html HTTP/1.1(CRLF)
+                ^
+             */
+            int d;
+            StringBuilder builder = new StringBuilder();
+            char pre='a',cur='a';//pre记录上次读取的字符 cur记录本次读取的字符
+            while((d = in.read())!=-1){
+                cur = (char)d;
+                if(pre==13&&cur==10){//判断是否连续读取到了回车+换行
+                    break;
+                }
+                builder.append(cur);
+                pre = cur;
+            }
+            String line = builder.toString();
+            System.out.println("请求行:"+line);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
