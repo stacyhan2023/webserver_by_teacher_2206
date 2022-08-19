86ca8d54ee92f1e39337b8562c1781c46c7ffd2f
jason <qlfreesky@gmail.com>
2022-08-19 17:29:34 +0800

    day08

--- V19/src/main/java/com/webserver/core/WebServerApplication.java
+++ V19/src/main/java/com/webserver/core/WebServerApplication.java
@@ -3,6 +3,8 @@ package com.webserver.core;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * WebServer web容器
@@ -10,10 +12,13 @@ import java.net.Socket;
  */
 public class WebServerApplication {
     private ServerSocket serverSocket;
+    private ExecutorService threadPool;
+
     public WebServerApplication(){
         try {
             System.out.println("正在启动服务端...");
             serverSocket = new ServerSocket(8088);
+            threadPool = Executors.newFixedThreadPool(50);
             System.out.println("服务端启动完毕!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -27,8 +32,7 @@ public class WebServerApplication {
                 System.out.println("一个客户端连接了!");
                 //启动一个线程处理该客户端的交互
                 ClientHandler handler = new ClientHandler(socket);
-                Thread t = new Thread(handler);
-                t.start();
+                threadPool.execute(handler);
             }
         } catch (IOException e) {
             e.printStackTrace();
